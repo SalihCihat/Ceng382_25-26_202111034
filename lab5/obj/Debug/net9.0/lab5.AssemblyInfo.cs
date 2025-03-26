f728df1cffbe8e9afc7d43bd5c49e7a2e4b85b88
Salih Cihat <salih06cihat@gmail.com>
2025-03-26 11:49:38 +0300

    week5 files added

--- lab5/obj/Debug/net9.0/lab5.AssemblyInfo.cs
+++ lab5/obj/Debug/net9.0/lab5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937b10e491c067f29c77a7bd2578aa7037abc5ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e71fde0df696196e674e84aa17fdae2120400de")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
